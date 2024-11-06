b847f68e9f7aedbe187b203b82cb8f364ef18bfc
BinaryNikita <169074248+BinaryNikita@users.noreply.github.com>
2024-11-06 06:36:07 +0000

    added department_id as foreign key in Employee table

--- AllSqlQueries.sql
+++ AllSqlQueries.sql
@@ -4,3 +4,6 @@ create table Employees (employee_id int primary key,   first_name varchar(50) no
 last_name varchar(50) not null, email varchar(100) unique not null, phone_number varchar(15), hire_date date not null, job_title varchar(50), salary decimal(10, 2) );
 
 create table Departments ( department_id int primary key auto_increment, department_name varchar(50) not null, manager_id int, foreign key (manager_id) references Employees(employee_id) );
+
+alter table Employees add column department_id int;
+alter table Employees add constraint department_id foreign key(department_id) references Departments(department_id);
