8a79adc90b7a0dc4fecbcd975c27fcfd672c939b
BinaryNikita <169074248+BinaryNikita@users.noreply.github.com>
2024-11-06 06:20:32 +0000

    created table Employees

--- AllSqlQueries.sql
+++ AllSqlQueries.sql
@@ -1,1 +1,4 @@
 create database Tech_Solutions;
+
+create table Employees (employee_id int primary key,   first_name varchar(50) not null,
+last_name varchar(50) not null, email varchar(100) unique not null, phone_number varchar(15), hire_date date not null, job_title varchar(50), salary decimal(10, 2) );
